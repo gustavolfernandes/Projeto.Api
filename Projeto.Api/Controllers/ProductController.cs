534892b86f39f234010f3ce83d9b360b0c097ba8
Gustavo Fernandes <gustavo.lino@desq.com.br>
2022-10-25 18:27:35 -0300

    feat: add mediatR in controller

--- Projeto.Api/Controllers/ProductController.cs
+++ Projeto.Api/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Projeto.Application.Products;
 
 namespace Projeto.Api.Controllers
@@ -8,24 +9,104 @@ namespace Projeto.Api.Controllers
     public class ProductsController : ControllerBase
     {
         [HttpGet("{id}")]
-        public Task<ProductResult> GetProductById([FromRoute] Guid id, [FromServices] ProductsQueryHandler handler)
-            => handler.RunAsync(new GetProductQuery { Id = id});
+        [ProducesResponseType(typeof(ProductResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetProductById([FromRoute] Guid id, 
+                                                        [FromServices] IMediator mediator,
+                                                        [FromServices] ILogger<ProductsController> _logger)
+        {
+            try
+            {
+                _logger.LogInformation($"Get details by productId: {id}");
+                var command = new GetProductQuery { Id = id };
+                var result =  await mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.Message);
+
+                return BadRequest();
+            }
+        }
 
         [HttpGet]
-        public Task<ProductResult[]> GetProducts([FromQuery] GetProductsQuery query, [FromServices] ProductsQueryHandler handler)
-            => handler.RunAsync(query);
+        [ProducesResponseType(typeof(ProductResult[]), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetProducts([FromServices] IMediator mediator,
+                                                        [FromServices] ILogger<ProductsController> _logger)
+        {
+            try
+            {
+                _logger.LogInformation($"Get products list");
+                var command = new GetProductsQuery();
+                var result = await mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.Message);
+
+                return BadRequest();
+            }
+        }
 
         [HttpPost]
-        public Task PostProducts([FromBody] PostProductCommand command, [FromServices] ProductsCommandHandler handler)
-            => handler.RunAsync(command);
+        [ProducesResponseType(typeof(ProductCommandResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> PostProduct([FromServices] IMediator mediator, [FromBody] PostProductCommand command, [FromServices] ILogger<ProductsController> _logger)
+        {
+            {
+                try
+                {
+                    _logger.LogInformation($"Creating a product with id: {command.Id}");
+                    var result = await mediator.Send(command);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(ex.Message);
+
+                    return BadRequest();
+                }
+            }
+        }
 
         [HttpPut]
-        public Task PutProduct([FromBody] PutProductCommand command, [FromServices] ProductsCommandHandler handler)
-            => handler.RunAsync(command);
+        [ProducesResponseType(typeof(ProductCommandResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> PutProduct([FromServices] IMediator mediator, [FromBody] PutProductCommand command, [FromServices] ILogger<ProductsController> _logger)
+        {
+            {
+                try
+                {
+                    _logger.LogInformation($"Updating a product with id: {command.Id}");
+                    var result = await mediator.Send(command);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(ex.Message);
 
-        [HttpDelete("{id}")]
-        public Task DeleteProduct([FromRoute] Guid id, [FromServices] ProductsCommandHandler handler)
-            => handler.RunAsync(new DeleteProductCommand { Id = id });
+                    return BadRequest();
+                }
+            }
+        }
 
+        [HttpDelete]
+        [ProducesResponseType(typeof(ProductCommandResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteProduct([FromServices] IMediator mediator, [FromBody] DeleteProductCommand command, [FromServices] ILogger<ProductsController> _logger)
+        {
+            {
+                try
+                {
+                    _logger.LogInformation($"Updating a product with id: {command.Id}");
+                    var result = await mediator.Send(command);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(ex.Message);
+
+                    return BadRequest();
+                }
+            }
+        }
     }
 }
