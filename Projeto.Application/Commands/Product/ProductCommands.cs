5edee18da5a4e124258920978ec44f5cee16feec
Gustavo Fernandes <gustavo.lino@desq.com.br>
2022-10-25 18:10:30 -0300

    feat: add command folder in application and add mediatR

---
+++ Projeto.Application/Commands/Product/ProductCommands.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Projeto.Domain;
+using Projeto.Infra;
+
+namespace Projeto.Application.Products;
+public sealed class PostProductCommand : IRequest<ProductCommandResponse>
+{
+    /// <example>336037D8-A93D-4401-8228-663E007DCBC4</example>
+    public Guid Id { get; set; }
+
+    /// <example>lorem ipsum</example>
+    public string Name { get; set; } = default!;
+
+    public void Deconstruct(out ProductId id, out string name)
+    {
+        id = Id.ToType<ProductId>();
+        name = Name ?? throw new AppException("Product name can't be null");
+    }
+}
+
+public sealed class PutProductCommand : IRequest<ProductCommandResponse>
+{
+    /// <example>0DDD2EAC-CEFC-49A8-A61E-A6B0FF4F4382</example>
+    public Guid Id { get; set; }
+
+    /// <example>lorem ipsum</example>
+    public string Name { get; set; } = default!;
+
+    public void Deconstruct(out ProductId id, out string name)
+    {
+        id = Id.ToType<ProductId>();
+        name = Name ?? throw new AppException("Product name can't be null");
+    }
+}
+
+public sealed class DeleteProductCommand : IRequest<ProductCommandResponse>
+{
+    public Guid Id { get; set; }
+}
+
+public class ProductCommandResponse
+{
+    public ProductCommandResponse(bool sucess)
+    {
+        Sucess = sucess;
+    }
+
+    public bool Sucess { get; set; }
+}
---
+++ Projeto.Application/Commands/Product/ProductCommandsHandler.cs
@@ -0,0 +1,59 @@
+﻿using Projeto.Domain.Interfaces;
+using Projeto.Infra;
+using Projeto.Domain;
+using Projeto.Application.Products.Services;
+using MediatR;
+
+namespace Projeto.Application.Products
+{
+    [Injectable(LifeTime.Scoped)]
+    public sealed class ProductsCommandHandler :
+        IRequestHandler<PostProductCommand, ProductCommandResponse>,
+        IRequestHandler<DeleteProductCommand, ProductCommandResponse>,
+        IRequestHandler<PutProductCommand, ProductCommandResponse>
+    {
+        public ProductsCommandHandler(IProductsRepository productsRepository, IProductsService productsService)
+        {
+            ProductsRepository = productsRepository;
+            ProductsService = productsService;
+        }
+
+        private IProductsRepository ProductsRepository { get; }
+        private IProductsService ProductsService { get; }
+
+        public async Task<ProductCommandResponse> Handle(PostProductCommand command)
+        {
+            var (id, name) = command;
+            ProductsRepository.Add(new Product(id, name));
+            await ProductsRepository.SaveChanges();
+            var response = new ProductCommandResponse(true);
+            return response;
+        }
+
+        public async Task<ProductCommandResponse> Handle(DeleteProductCommand command)
+        {
+            var id = command.Id.ToType<ProductId>();
+
+            var entity = await ProductsService.FindById(id);
+            if (entity is null) throw new AppException("Product not found");
+
+            ProductsRepository.Remove(entity);
+            await ProductsRepository.SaveChanges();
+            var response = new ProductCommandResponse(true);
+            return response;
+        }
+
+        public async Task<ProductCommandResponse> RunAsync(PutProductCommand command)
+        {
+            var (id, name) = command;
+
+            var entity = await ProductsService.FindById(id);
+            if (entity is null) throw new AppException("Product not found");
+
+            entity.Update(name);
+            await ProductsRepository.SaveChanges();
+            var response = new ProductCommandResponse(true);
+            return response;
+        }
+    }
+}
