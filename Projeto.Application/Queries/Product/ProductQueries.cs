1c7fcd531724696d54d750bdecb3690ee1dffb3a
Gustavo Fernandes <gustavo.lino@desq.com.br>
2022-10-25 18:10:14 -0300

    feat: add queries folder in application and add mediatR

---
+++ Projeto.Application/Queries/Product/ProductQueries.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Projeto.Domain;
+
+namespace Projeto.Application.Products
+{
+    public sealed class GetProductsQuery : IRequest<ProductResult[]>
+    {
+
+    }
+    public sealed class GetProductQuery : IRequest<ProductResult>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public sealed class ProductResult
+    {
+        public ProductResult(Product entity)
+        {
+            Id = entity.Id.Guid;
+            Name = entity.Name;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Projeto.Application/Queries/Product/ProductQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Projeto.Application.Products.Services;
+using Projeto.Domain;
+using Projeto.Infra;
+
+namespace Projeto.Application.Products
+{
+    [Injectable(LifeTime.Scoped)]
+    public sealed class ProductsQueryHandler :
+        IRequestHandler<GetProductsQuery, ProductResult[]>,
+        IRequestHandler<GetProductQuery, ProductResult>
+    {
+        public ProductsQueryHandler(IProductsService productsService)
+        {
+            ProductsService = productsService;
+        }
+
+        private IProductsService ProductsService { get; }
+
+        public async Task<ProductResult[]> Handle(GetProductsQuery query)
+        {
+            var items = await ProductsService.FindAll();
+            return items.Select(e => new ProductResult(e)).ToArray();
+        }
+        public async Task<ProductResult> Handle(GetProductQuery query)
+        {
+            var id = query.Id.ToType<ProductId>();
+            var entity = await ProductsService.FindById(id);
+            if (entity is null) throw new AppException("Product not found");
+
+            return new ProductResult(entity);
+        }
+    }
+}
