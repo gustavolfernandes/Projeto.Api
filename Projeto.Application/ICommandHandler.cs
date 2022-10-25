5caee27706a43c18af7b630015c3070b1da3dd56
Gustavo Fernandes <gustavo.lino@desq.com.br>
2022-10-25 18:09:03 -0300

    refactor: remove query and command handler interface to add mediatR

--- Projeto.Application/ICommandHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Projeto.Application
-{
-    public interface ICommand { }
-
-    public interface ICommandHandler<in TRequest> where TRequest : ICommand
-    {
-        Task RunAsync(TRequest request);
-    }
-}
